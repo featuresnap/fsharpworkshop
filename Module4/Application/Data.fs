4ec0196935a6e7e083bf2947cde12e95bf443450
Patrick Kelly <patrick.kelly@sherwin.com>
2016-10-20 12:10:52 -0400

    Exercise 4 step 5.4

--- Module4/Application/Data.fs
+++ Module4/Application/Data.fs
@@ -9,3 +9,16 @@ let getSpendings id =
     |> Seq.filter (fun c -> c.Id = id)
     |> Seq.collect (fun c -> c.Spendings)
     |> List.ofSeq
+
+type Csv = CsvProvider<"Data.csv">
+let getCustomers () =
+    let file = Csv.Load "Data.csv"
+    file.Rows
+    |> Seq.map (fun c -> 
+    {
+        Id = c.Id;
+        IsVip = c.IsVip;
+        Credit = c.Credit * 1M<USD>;
+        PersonalDetails = None;
+        Notifications = NoNotifications
+    })
