8d9029eb9bfae0763d3a9cd3bb07ffcf25726b9d
Patrick <pkelly14159@hotmail.com>
2016-10-20 06:14:06 -0400

    Exercise 3 step 1.2

--- Module3/Application/Types.fs
+++ Module3/Application/Types.fs
@@ -2,8 +2,23 @@
 
 open System
 
+type PersonalDetails = {
+    FirstName:string;
+    LastName:string;
+    DateOfBirth:DateTime
+}
+
+type Notifications = 
+    |NoNotifications
+    |ReceiveNotifications of receiveDeals:bool * receiveAlerts:bool
+
+[<Measure>] type AUD
+[<Measure>] type USD
+
 type Customer = { 
     Id: int
     IsVip: bool
-    Credit: decimal 
+    Credit: decimal<USD>
+    PersonalDetails: PersonalDetails option
+    Notifications: Notifications
 }
